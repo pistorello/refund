d76d2aeb505dbd73d9aa61d2cd309c76e8873369
pistorello <leopmatos0605@outlook.com>
2025-12-23 08:37:08 -0300

    feat: created text component

--- src/components/text.tsx
+++ src/components/text.tsx
@@ -1,7 +1,7 @@
 import { createElement, type JSX, type ReactNode } from "react";
 import { tv, type VariantProps } from "tailwind-variants";
 
-export const textVariants = tv({
+const textVariants = tv({
   base: "font-sans text-gray-100",
   variants: {
     variant: {
@@ -9,13 +9,13 @@ export const textVariants = tv({
       "heading-medium": "text-lg leading-5 font-bold",
       "heading-small": "text-sm leading-4 font-bold",
       "paragraph-medium": "text-base leading-4 font-medium",
-      "label-small": "text-sm leading-4 font-light uppercase",
-    }
+      "label-small": "text-xs leading-4 font-light uppercase",
+    },
   },
   defaultVariants: {
     variant: "paragraph-medium",
   },
-})
+});
 
 interface TextProps extends VariantProps<typeof textVariants> {
   as?: keyof JSX.IntrinsicElements;
@@ -34,7 +34,7 @@ export default function Text({
     as,
     {
       className: textVariants({ variant, className }),
-      ...props
+      ...props,
     },
     children
   );

b661e4369991795d9de7fbac61c1243053e8765d
pistorello <leopmatos0605@outlook.com>
2025-12-23 08:35:50 -0300

    feat: finish button component

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -1,14 +1,50 @@
-import { tv } from "tailwind-variants";
+import type { ComponentProps } from "react";
+import { tv, type VariantProps } from "tailwind-variants";
+import Text from "./text";
 
-export const buttonVariants = tv({
-  base: "cursor-pointer rounded-lg px-5",
+const buttonVariants = tv({
+  base: "cursor-pointer select-none",
   variants: {
     variant: {
       primary: `bg-green-100 hover:bg-green-200`,
     },
     size: {
-      md: "w-full h-16",
+      md: "w-full h-12 rounded-lg px-5",
     },
-    disabled: "pointer-events-none opacity-70",
+    disabled: {
+      true: "pointer-events-none opacity-50",
+    },
+  },
+  defaultVariants: {
+    variant: "primary",
+    size: "md",
+    disabled: false,
   },
 });
+
+interface ButtonProps
+  extends VariantProps<typeof buttonVariants>,
+    Omit<ComponentProps<"button">, "size" | "disabled"> {
+  children: React.ReactNode;
+  disabled?: boolean;
+}
+
+export default function Button({
+  variant,
+  size,
+  disabled,
+  className,
+  children,
+  ...props
+}: ButtonProps) {
+  return (
+    <button
+      className={buttonVariants({ variant, size, disabled, className })}
+      {...props}
+    >
+      <Text as="label" variant="heading-small" className="text-white">
+        {children}
+      </Text>
+    </button>
+  );
+}

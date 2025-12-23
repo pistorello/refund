19cd41bd8d6f643d74cf526b82b5c90f4cd5cb84
pistorello <leopmatos0605@outlook.com>
2025-12-23 08:39:41 -0300

    feat: add new components to app page

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
+import SearchIcon from "./assets/images/icons/magnifying-glass.svg?react";
+
+import Button from "./components/button";
+import IconButton from "./components/icon-button";
+import InputText from "./components/input-text";
 import Text from "./components/text";
 
 export default function App() {
   return (
-    <main>
+    <main className="space-y-5 px-4">
       <div className="flex flex-col gap-2">
         <Text variant="heading-large">Hello World!</Text>
         <Text variant="heading-medium">Hello World!</Text>
@@ -11,7 +16,21 @@ export default function App() {
         <Text variant="paragraph-medium">Hello World!</Text>
       </div>
 
-      <div className="flex flex-col gap-10"></div>
+      <div className="flex gap-3">
+        <Button>Button</Button>
+        <Button disabled>Button</Button>
+      </div>
+
+      <div className="flex gap-3">
+        <IconButton icon={SearchIcon} />
+        <IconButton icon={SearchIcon} disabled />
+      </div>
+
+      <div>
+        <InputText variant="primary" placeholder="Título...">
+          Título
+        </InputText>
+      </div>
     </main>
   );
 }
